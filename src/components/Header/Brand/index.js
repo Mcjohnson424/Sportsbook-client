9e37a37998662cbfffd8eb39a7e58e302ab55fd3
Dax <daaawx@gmail.com>
2021-01-17 18:49:58 +0100

    Minor changes in logo

--- src/components/Header/Brand/index.js
+++ src/components/Header/Brand/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Logo from '../../Logo';
+import {Link} from 'react-router-dom';
+import * as ROUTES from '../../../constants/routes';
 
 export default function Brand() {
   return (
-      <div className="top-bar-brand d-block" style={{width: '75px', paddingTop: '11px'}}>
-        <Logo height="25" color="#afc0ff"/>
+      <div className="top-bar-brand d-block"
+           style={{width: '75px', paddingTop: '11px'}}>
+        <Link to={ROUTES.DASHBOARD.as()}>
+          <Logo height="25" color="#afc0ff"/>
+        </Link>
       </div>
   );
 }
